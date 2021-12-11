6f460cf1cb525bd82663c79064b4d55a916c60f8
Knarlus <71461256+Knarlus@users.noreply.github.com>
2021-12-11 12:02:27 +0100

    get_ip function

--- KnarlusGetIp.py
+++ KnarlusGetIp.py
@@ -1,2 +1,9 @@
 import json
 import requests
+
+ipv4_tool = "https://iptools-4.top10vpn.com/ip/"
+
+
+def get_ip(url: str = ipv4_tool) -> str:
+    ip_string = requests.get(url=url)
+    return json.loads(ip_string.content.decode("utf-8"))["ip"]
