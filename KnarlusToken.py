97e93b6d71b401ca82b7807f6fa6805be0673bf9
Knarlus <71461256+Knarlus@users.noreply.github.com>
2021-12-11 12:41:14 +0100

    get-token updated

--- KnarlusToken.py
+++ KnarlusToken.py
@@ -1,4 +1,16 @@
+import KnarlusLogKonsole as KnLog
+
+
 def get_token(relative_file_path: str = "TOKEN.txt") -> str:
-    with open(file=relative_file_path, mode="r") as token_file:
-        token = token_file.readline()
-    return token
+    try:
+        with open(file=relative_file_path, mode="r") as token_file:
+            token = token_file.readline()
+        return token
+    except FileNotFoundError as error:
+        KnLog.log_console(log_msg="The file {str} was not found. Please set up the token file!",
+                          log_function_name="get_token", log_type="err")
+        exit(1)
+
+
+if __name__ == "__main__":
+    print(get_token())
