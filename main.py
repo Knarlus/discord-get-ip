6b66c23e1faaa2cdc555d204f6716e7ef65fa5bc
Knarlus <71461256+Knarlus@users.noreply.github.com>
2021-12-11 19:04:24 +0100

    cleaned imports fixed multiline strings in log msgs

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from discord.ext import commands
 from discord_slash import SlashCommand  # pip install discord-py-slash-command
 
 from KnarlusGetIp import get_ip
-from KnarlusLogConsole import log_to_console, log_to_file
-from KnarlusReadConfig import get_token
+from KnarlusLogConsole import log_to_console
+from KnarlusReadConfig import get_token, get_guild_ids
 
-import discord
 
-from datetime import datetime, timedelta
+from datetime import datetime
 
 token = get_token()
 
-guild_ids: list  # = None  # TODO import guild id's via KnarlusReadConfig.py
+guild_ids = get_guild_ids()
 
 bot = commands.Bot("¥")
 slash = SlashCommand(bot)
@@ -27,17 +26,17 @@ async def on_ready():
     for guild in bot.guilds:
         if guild.id in guild_ids:
             log_to_console(
-                log_msg=f"bot is member of guild {guild.name} (id {guild.id}) and has the id already registered,\
-                therefore has slash commands enabled", log_function_name="on_ready", log_type="inf")
+                log_msg=f"bot is member of guild {guild.name} (id {guild.id}) and has the id already registered, \
+therefore has slash commands enabled", log_function_name="on_ready", log_type="inf")
         else:
             log_to_console(
-                log_msg=f"bot is member of guild {guild.name} (id {guild.id}) but not yet registered, enabling\
-                slash commands for this startup", log_function_name="on_ready", log_type="war")
+                log_msg=f"bot is member of guild {guild.name} (id {guild.id}) but not yet registered, enabling \
+slash commands for this startup", log_function_name="on_ready", log_type="war")
             guild_ids.append(guild.id)
 
 
 @slash.slash(name="ip", description="print the ipv4 to connect to the minecraft server", guild_ids=guild_ids, options=
-[{"name": "force_reload", "description": "forces ip reload in case the program did use the last one, assuming it has\
+[{"name": "force_reload", "description": "forces ip reload in case the program did use the last one, assuming it has \
 not changed since the last request\n0 => no force (just like not giving this), any other number => force reload",
   "required": False, "type": 4}])
 async def ip(ctx, force_reload: int = 0):
