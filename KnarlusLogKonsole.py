56360d0b3c138a43d8aafc330c7348fd7b0d9812
Knarlus <71461256+Knarlus@users.noreply.github.com>
2021-12-11 13:38:36 +0100

    KnarluLogKonsole.py usage of dict instead of if elif else

--- KnarlusLogKonsole.py
+++ KnarlusLogKonsole.py
@@ -1,26 +1,31 @@
 from datetime import datetime
 from termcolor import colored, cprint
 
-def log_console(log_msg: str, log_function_name: str, log_type: str):
-    current_datetime = datetime.now()
-    current_time = current_datetime.strftime("%d.%m.%Y @ %H:%M:%S")
-    if not isinstance(log_msg, str):
-        log_msg = str(log_msg)
+log_types = {"inf": ("[ Info  ]", "green"), "war": ("[Warning]", "yellow"), "err": ("[ Error ]", "red"),
+             "otherwise": ("[ None  ]", "grey")}
+
+
+def log_to_console(log_msg: str, log_function_name: str, log_type: str, write_to_file: bool = False):
+    current_time = datetime.now().strftime("%d.%m.%Y @ %H:%M:%S")
     log_type = log_type.lower()
-    if log_type == "err":
-        print_out = colored(str(f"[ ERROR ] {current_time} | {log_function_name} | {log_msg}"), "red")
-    elif log_type == "war":
-        print_out = colored(str(f"[WARNING] {current_time} | {log_function_name} | {log_msg}"),"yellow")
-    elif log_type == "inf":
-        print_out = colored(str(f"[ INFO  ] {current_time} | {log_function_name} | {log_msg}"), "green")
+    if log_type in log_types.keys():
+        print_type, log_color = log_types[log_type]
     else:
-        log_error = "Function 'log_console()' was called with invalid log_type: " + str(log_type)
-        log_console(log_msg=log_error, log_function_name=log_function_name, log_type="war")
-        print_out = colored(str(f"[ NONE  ] {current_time} | {log_function_name} | {log_msg}"), "grey")
-    cprint(print_out)
+        print_type, log_color = log_types["otherwise"]
+        log_to_console(log_msg=f"function was called with illegal parameter for 'log_type': '{log_type}'",
+                       log_function_name="log_to_console", log_type="war", write_to_file=write_to_file)
+    cprint(colored(f"{print_type} {current_time} | {log_function_name} | {log_msg}", log_color))
+    if write_to_file:
+        log_to_file(log_msg=log_msg, log_function_name=log_function_name, log_type=log_type)
 
-def log_file(print_out):
-    "write to file"
+
+def log_to_file(log_msg: str, log_function_name: str, log_type: str, log_path: str = "log.txt"):
+    current_time = datetime.now().strftime("%d.%m.%Y @ %H:%M:%S")
+    log_type = log_type.lower()
+    print_type, _ = log_types[log_type] if log_type in log_types.keys() else log_types["otherwise"]
+    with open(file=log_path, mode="a") as log_file:
+        log_file.write(f"{print_type} {current_time} | {log_function_name} | {log_msg}\n")
 
 
-log_console("Not available", "Discord-API", "err")
+if __name__ == "__main__":
+    log_to_console("Executed as main script", "KnarlusLogConsole.py", "err", True)
--- KnarlusToken.py
+++ KnarlusToken.py
@@ -13,10 +13,10 @@ def get_token(file_path: str = "TOKEN.txt") -> str:
             token = token_file.readline()
         return token
     except FileNotFoundError as error:
-        KnLog.log_console(log_msg=f"The file '{file_path}' was not found. Please set up the token file!",
-                          log_function_name="get_token", log_type="err")
+        KnLog.log_to_console(log_msg=f"The file '{file_path}' was not found. Please set up the token file!",
+                             log_function_name="get_token", log_type="err")
         exit(1)
 
 
 if __name__ == "__main__":
-    print(get_token())
+    print(get_token("tok"))
